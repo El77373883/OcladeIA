915d3e24f4e357368024bdf42ed3c8c3cbc87724
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-04-01 21:04:51 -0600

    Update and rename ui/animations.js to js/ui/animations.js

---
+++ js/ui/animations.js
@@ -0,0 +1,7 @@
+function showThinking() {
+  document.getElementById('thinking-container').classList.remove('hidden');
+}
+
+function hideThinking() {
+  document.getElementById('thinking-container').classList.add('hidden');
+}
