a8e706b539b7f0bee318efdd4b2b7bb619f67258
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-04-01 21:25:47 -0600

    Update tokenizer.js

--- core/tokenizer.js
+++ core/tokenizer.js
@@ -1,1 +1,5 @@
-
+function updateTokenCounter(length) {
+  tokenCount += Math.floor(length / 4);
+  if (tokenCount > 4000) tokenCount = 4000;
+  document.querySelector('.token-counter').textContent = `${tokenCount} / 4000 tokens`;
+}
